c4ec6f89109ae689188483f9c84c88679e099f15
OzerK <ozerkerr@gmail.com>
2022-07-26 02:35:02 -0700

    add api for openweather

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,10 @@
+// grab DOM references
 const searchDiv = $('#search-table');
 const mainContet = $('#main-content');
 
+// create global vars
+const storedCities = JSON.parse(localStorage.getItem("cities")) || [];
+const apiKey = "2c81cdc8d272d546f790d65b4c95f8c9";
 // function to render search bar
 function createSearchBar() {
   // create elements
@@ -8,6 +12,7 @@ function createSearchBar() {
   const searchBar = $('<input>');
   const searchButton = $('<button>');
 
+  // set attributes
   searchBar.attr("type", "search");
   searchBar.attr("class", "form-control rounded d-flex flex-row");
   searchBar.attr("placeholder", "City name");
@@ -21,37 +26,83 @@ function createSearchBar() {
   searchButton.text("Search");
 
   searchButton.click(function () {
+    // Call search City function with search value 
     searchCity($('#search-bar').val());
+    // create new list item of search history
+    const listGroup = $('.list-group')
+    const listItem = $('<a>');
+
+    // add class for list item
+    listItem.attr("class", "list-group-item list-group-item-action px-3 border-0 rounded-3 mb-2 list-group-item-info d-flex justify-content-center")
+    // add text
+    listItem.text($('#search-bar').val());
+    // append to div element
+    listGroup.prepend(listItem)
     $('#search-bar').val('');
   })
 
   // append children to html
   inputGroup.append(searchBar, searchButton);
   searchDiv.append(inputGroup);
-}
+};
 
+// search function which also stores value into local storage
 function searchCity(city) {
-  const storedCities = JSON.parse(localStorage.getItem("cities")) || [];
-
   storedCities.push(city);
 
+  // save to local storage
   localStorage.setItem("cities", JSON.stringify(storedCities));
-}
+  getLocationApi(city)
+};
 
+// create function to get lat and long based on city name
+function getLocationApi(param) {
+  const city = param.replace(' ', '+');
+  const requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
+  fetch(requestUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          getWeatherApi(data[0].lat, data[0].lon)
+        })
+      }
+  });
+};
+
+// create function to get weather data based on lat and lon
+function getWeatherApi(lat, lon) {
+  console.log(lat, lon, "robin");
+  const requestUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+
+  fetch(requestUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log(data, 'hancock')
+        })
+      }
+    });
+
+
+};
+
+// render list of search history  
 function renderSearchedCitiesList() {
-  const listGroup = $('<ul>');
-  const storedCities = JSON.parse(localStorage.getItem("cities")) || [];
-
+  // create div element and add attribute
+  const listGroup = $('<div>');
   listGroup.attr("class", "list-group list-group-light");
 
-  storedCities.map((city) => {
-    const listItem = $('<li>');
+  // map through array and reverse and return list items
+  storedCities.slice(0).reverse().map((city) => {
+    const listItem = $('<a>');
 
+    listItem.attr("class", "list-group-item list-group-item-action px-3 border-0 rounded-3 mb-2 list-group-item-info d-flex justify-content-center")
     listItem.text(city);
 
     listGroup.append(listItem);
   })
 
+  // append to DOM
   searchDiv.append(listGroup);
 }
 
